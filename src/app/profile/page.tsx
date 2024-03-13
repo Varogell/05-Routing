cb7a0afb26b49f69d0bf8b9df10e60c21b3cbf9a
Varogell <alvaroivanka@gmail.com>
2024-03-13 13:45:37 +0700

    Praktikum 1

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,10 @@
+export default function Profile() {
+    return (
+        <div>
+        <h1>Selamat datang</h1>
+        <h2>Nama: Alvaro Hegel Ivanka</h2>
+        <h2>Kelas: 3A</h2>
+        <h2>Nim  : 2141720237</h2>
+        </div>
+    );
+}
